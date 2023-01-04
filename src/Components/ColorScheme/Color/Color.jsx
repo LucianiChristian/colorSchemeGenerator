0e24009eaeabb0da49257f964cb3b444f4987e2c
Christian Luciani <luciani_christian@yahoo.com>
2023-01-03 22:43:56 -0600

    Add click to copy functionality w/ animation

--- src/Components/ColorScheme/Color/Color.jsx
+++ src/Components/ColorScheme/Color/Color.jsx
@@ -1,15 +1,30 @@
+import { useState } from "react";
 import classes from "./Color.module.css";
 
 export default function Color({hex, name}) {
+    const [ animate, setAnimate ] = useState(false);
+
+    function copyHexToClipboard() {
+        navigator.clipboard.writeText(hex);
+    }
+
     return (
-        <div className={classes.container}>
+        <div 
+            className={classes.container}
+            onClick={copyHexToClipboard}>
             <div 
                 className={classes.colorBlock} 
                 style={{backgroundColor: hex}}
             >
                 <div className={classes.colorName}>{name}</div>    
             </div>
-            <footer className={classes.hexFooter}>{hex}</footer>
+            <footer className={classes.hexFooter} onClick={() => setAnimate(true)}>
+                {hex}
+                <div 
+                    className={`${classes.hexFooterCopied} ${animate ? classes.fadeUp : ""}`}
+                    onAnimationEnd={() => setAnimate(false)}   
+                >copied</div>
+            </footer>
         </div>
     )
 }
